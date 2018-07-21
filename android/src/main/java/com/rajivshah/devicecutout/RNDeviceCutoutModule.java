18ffb1813138ac5492c19f3a08a4d818107c1d58
Rajiv Shah <rajivshah1@icloud.com>
2018-07-21 16:22:03 -0400

    Cleanup (#6)
    * Cleanup
    
    * Remove unused import

--- android/src/main/java/com/rajivshah/devicecutout/RNDeviceCutoutModule.java
+++ android/src/main/java/com/rajivshah/devicecutout/RNDeviceCutoutModule.java
@@ -1,9 +1,6 @@
 
 package com.rajivshah.devicecutout;
 
-import android.support.annotation.NonNull;
-import android.app.Activity;
-import android.util.Log;
 import android.view.WindowManager.LayoutParams;
 import android.view.DisplayCutout;
 import android.graphics.Rect;
@@ -17,23 +14,10 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Promise;
 
-import java.io.IOException;
-import java.util.List;
-import java.nio.charset.StandardCharsets;
-import java.lang.IllegalArgumentException;
-import java.lang.Error;
-
 public class RNDeviceCutoutModule extends ReactContextBaseJavaModule {
-
-  private final ReactApplicationContext reactContext;
-  private final ReactApplicationContext baseContext;
-  private final Activity activity;
 
   public RNDeviceCutoutModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.reactContext = reactContext;
-    this.baseContext = getReactApplicationContext();
-    this.activity = getCurrentActivity();
   }
 
   @Override
