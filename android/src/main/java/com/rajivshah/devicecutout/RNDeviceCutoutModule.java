3a0dceedf59b292e06012f2539c621a99ab7747c
Rajiv Shah <rajivshah1@icloud.com>
2018-07-21 16:00:58 -0400

    Write native methods

--- android/src/main/java/com/rajivshah/devicecutout/RNDeviceCutoutModule.java
+++ android/src/main/java/com/rajivshah/devicecutout/RNDeviceCutoutModule.java
@@ -4,6 +4,9 @@ package com.rajivshah.devicecutout;
 import android.support.annotation.NonNull;
 import android.app.Activity;
 import android.util.Log;
+import android.view.WindowManager.LayoutParams;
+import android.view.DisplayCutout;
+import android.graphics.Rect;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -38,6 +41,103 @@ public class RNDeviceCutoutModule extends ReactContextBaseJavaModule {
     return "RNDeviceCutout";
   }
 
-  
+  /**
+   * Sets mode to default (0)
+   * See https://developer.android.com/reference/android/R.attr#windowLayoutInDisplayCutoutMode
+   * @param promise
+   */
+  @ReactMethod
+  public void layoutInDisplayCutoutMode(final Promise promise) {
+    try {
+      LayoutParams.layoutInDisplayCutoutMode();
+      promise.resolve();
+    } catch(Exception e) {
+      promise.reject(e);
+    }
+  }
+
+  /**
+   * Sets mode
+   * See https://developer.android.com/reference/android/R.attr#windowLayoutInDisplayCutoutMode
+   * @param mode
+   * @param promise
+   */
+  @ReactMethod
+  public void layoutInDisplayCutoutMode(final int mode, final Promise promise) {
+    try {
+      LayoutParams.layoutInDisplayCutoutMode(mode);
+      promise.resolve();
+    } catch(Exception e) {
+      promise.reject(e);
+    }
+  }
+
+  /**
+   * Gets bounding rectangles for non-functional areas on the display
+   * See https://developer.android.com/reference/android/view/DisplayCutout.html#getBoundingRects()
+   * @param promise
+   */
+  @ReactMethod
+  public void getBoundingRects(final Promise promise) {
+    List<Rect> rects = DisplayCutout.getBoundingRects();
+    if (rects.isEmpty()) {
+      promise.resolve(null);
+    } else {
+      WritableArray rectsArray = new WritableNativeArray();
+      for (Rect rect : rects) {
+        WritableMap rectangle = new WritableNativeMap();
+        rectangle.putInt("top", rect.top);
+        rectangle.putInt("bottom", rect.bottom);
+        rectangle.putInt("left", rect.left);
+        rectangle.putInt("right", rect.right);
+        rectsArray.pushMap(rectangle);
+      }
+      promise.resolve(rectsArray);
+    }
+  }
+
+  /**
+   * Gets inset from the bottom which avoids the cutout
+   * Represented as pixels
+   * See https://developer.android.com/reference/android/view/DisplayCutout.html#getSafeInsetBottom()
+   * @param promise
+   */
+  @ReactMethod
+  public void getSafeInsetBottom(final Promise promise) {
+    promise.resolve(DisplayCutout.getSafeInsetBottom());
+  }
+
+  /**
+   * Gets inset from the left which avoids the cutout
+   * Represented as pixels
+   * See https://developer.android.com/reference/android/view/DisplayCutout.html#getSafeInsetLeft()
+   * @param promise
+   */
+  @ReactMethod
+  public void getSafeInsetLeft(final Promise promise) {
+    promise.resolve(DisplayCutout.getSafeInsetLeft());
+  }
+
+  /**
+   * Gets inset from the right which avoids the cutout
+   * Represented as pixels
+   * See https://developer.android.com/reference/android/view/DisplayCutout.html#getSafeInsetRight()
+   * @param promise
+   */
+  @ReactMethod
+  public void getSafeInsetRight(final Promise promise) {
+    promise.resolve(DisplayCutout.getSafeInsetRight());
+  }
+
+  /**
+   * Gets inset from the top which avoids the cutout
+   * Represented as pixels
+   * See https://developer.android.com/reference/android/view/DisplayCutout.html#getSafeInsetTop()
+   * @param promise
+   */
+  @ReactMethod
+  public void getSafeInsetTop(final Promise promise) {
+    promise.resolve(DisplayCutout.getSafeInsetTop());
+  }
 
 }
